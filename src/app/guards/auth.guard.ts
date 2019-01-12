f4206238f68f084753d1b9197a4eb8cd23c36d9f
hoshymo <bezelplat@gmail.com>
2019-01-12 23:49:10 +0900

    initial

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean> | Promise<boolean> | boolean {
+    console.log('AuthGuard#canActivate');
+
+    console.log('user: ', this.authService.getAuthenticatedUser());
+    if (!this.authService.getAuthenticatedUser())
+      return false;
+
+    return true;
+  }
+}
