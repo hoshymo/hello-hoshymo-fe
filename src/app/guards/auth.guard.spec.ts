9b66924c85d01c639a9af28d9f776caddc69f815
hoshymo <hoshymo@hazm.in>
2019-01-19 15:59:15 +0900

    fixes for ng test

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,11 +1,22 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { FirebaseOptionsToken } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../../environments/environment';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [
+        RouterTestingModule,
+        AngularFireAuthModule
+      ],
+      providers: [
+        { provide: FirebaseOptionsToken, useValue: environment.firebase },
+      ]
     });
   });
 
